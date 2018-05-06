14ad7b6f69d93fafc19cdbed2618a6e7b63ca1f1
DABH <DABH@users.noreply.github.com>
2018-05-05 21:27:39 -0700

    try to find .LAST_GREEN_COMMIT

--- last_green_commit.sh
+++ last_green_commit.sh
@@ -4,5 +4,5 @@ pushd ${DIRNAME}
 url="${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines?private_token=${PRIVATE_TOKEN}&status=success&ref=${CI_COMMIT_REF_NAME}"
 commit=$(curl -k -s ${url} | jq -r -f .monorepo.gitlab/jq.filter)
 echo "Last green commit is '${commit}'."
-echo ${commit} > .LAST_GREEN_COMMIT
+echo ${commit} > .monorepo.gitlab/.LAST_GREEN_COMMIT
 popd
