7a317d60d368f26689b4f0b13378ad298f235428
DABH <DABH@users.noreply.github.com>
2018-05-05 21:25:09 -0700

    try to find jq.filter

--- last_green_commit.sh
+++ last_green_commit.sh
@@ -2,7 +2,7 @@
 DIRNAME="$(dirname $(readlink -f "$0"))"
 pushd ${DIRNAME}
 url="${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines?private_token=${PRIVATE_TOKEN}&status=success&ref=${CI_COMMIT_REF_NAME}"
-commit=$(curl -k -s ${url} | jq -r -f jq.filter)
+commit=$(curl -k -s ${url} | jq -r -f .monorepo.gitlab/jq.filter)
 echo "Last green commit is '${commit}'."
 echo ${commit} > .LAST_GREEN_COMMIT
 popd
